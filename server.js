9a083359bcdba008bc0907258cabf01cc076bcf4
Kanna <heddi932@gmail.com>
2019-12-04 11:26:17 +0100

    Merde

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var app = require('./app');
 
 var port = process.env.PORT || 3000; 	  // set our port
-var host = process.env.HOST || 'localhost'; // For Heroku to run successfully
+var host = process.env.HOST || '0.0.0.0'; // For Heroku to run successfully
 
-http.createServer(app).listen(port, () => {
-	console.log("Listening on port " + port);
+http.createServer(app).listen(port, host, () => {
+	console.log("Server ready at http://" + host + ":" + port);
 });
