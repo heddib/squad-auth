b410d532c087cee82c2cbfab584ede159514bc6d
Kanna <heddi932@gmail.com>
2019-12-04 11:25:52 +0100

    Test

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = require('./app');
 var port = process.env.PORT || 3000; 	  // set our port
 var host = process.env.HOST || 'localhost'; // For Heroku to run successfully
 
-http.createServer(app).listen(port, host, () => {
-	console.log("Server ready at http://" + host + ":" + port);
+http.createServer(app).listen(port, () => {
+	console.log("Listening on port " + port);
 });
