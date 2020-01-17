902c3ae810727aacbf7ec639ca333fbebd2fbba9
Kanna <heddi932@gmail.com>
2020-01-17 15:52:46 +0100

    Update profile service

--- users/user.service.js
+++ users/user.service.js
@@ -5,9 +5,9 @@ const sql = require("../db");
 const bcrypt = require("bcryptjs");
 const Validator = require("Validator");
 const saltRounds = 5;
-const salt = bcrypt.genSaltSync(saltRounds);
 
 let users = [];
+let profiles = [];
 
 /*const users = () => {
     return new Promise(function(resolve, reject) {
@@ -20,161 +20,319 @@ let users = [];
 };*/
 
 module.exports = {
-    authenticate,
-    register,
-    getAll,
-    getUserByName,
+  authenticate,
+  register,
+  getAll,
+  getUserByName,
+  getProfileByName,
+  updateProfileByName
 };
 
 async function populateUsers() {
-    let promise = new Promise(function(resolve, reject) {
-        sql.query("SELECT * FROM users", function(err, data) {
-            if (!err) resolve(JSON.parse(JSON.stringify(data)));
-            // Hacky solution
-            else reject(err);
-        });
+  let promise = new Promise(function(resolve, reject) {
+    sql.query("SELECT * FROM users", function(err, data) {
+      if (!err) resolve(JSON.parse(JSON.stringify(data)));
+      // Hacky solution
+      else reject(err);
     });
-    promise.then(u => {
-        users = u;
+  });
+  promise.then(u => {
+    users = u;
+  });
+  return promise;
+}
+
+async function populateProfiles() {
+  let promise = new Promise(function(resolve, reject) {
+    sql.query("SELECT * FROM profiles", function(err, data) {
+      if (!err) resolve(JSON.parse(JSON.stringify(data)));
+      // Hacky solution
+      else reject(err);
     });
-    return promise;
+  });
+  promise.then(p => {
+    profiles = p;
+  });
+  return promise;
 }
 
 async function registerUser(user) {
-    let promise = new Promise(function(resolve, reject) {
-        sql.query("INSERT INTO users SET ?", user, function(err, data) {
-            if (!err) resolve(true);
-            // Hacky solution
-            else reject(err);
-        });
+  let promise = new Promise(function(resolve, reject) {
+    sql.query("INSERT INTO users SET ?", user, function(err, data) {
+      if (!err) resolve(true);
+      // Hacky solution
+      else reject(err);
     });
-    return promise;
+  });
+  return promise;
+}
+
+async function registerProfile(user) {
+  let promise = new Promise(function(resolve, reject) {
+    sql.query("INSERT INTO profiles SET ?", user, function(err, data) {
+      if (!err) resolve(true);
+      // Hacky solution
+      else reject(err);
+    });
+  });
+  return promise;
+}
+
+async function updateProfile(username, profile) {
+  let promise = new Promise(function(resolve, reject) {
+    sql.query(
+      "UPDATE profiles SET ? WHERE username = ?",
+      [profile, username],
+      function(err, data) {
+        if (!err) {
+            userUpdated(username);
+            resolve(true);
+        } 
+        // Hacky solution
+        else reject(err);
+      }
+    );
+  });
+  return promise;
+}
+
+async function userUpdated(username) {
+  let user = {};
+  user.updated_at = new Date();
+  let promise = new Promise(function(resolve, reject) {
+    sql.query(
+      "UPDATE users SET ? WHERE username = ?",
+      [user, username],
+      function(err, data) {
+        if (!err) resolve(true);
+        // Hacky solution
+        else reject(err);
+      }
+    );
+  });
+  return promise;
 }
 
 async function authenticate({ username, password }) {
-    await populateUsers();
+  await populateUsers();
 
-    //console.log('Replace with : ' + bcrypt.hashSync(password, saltRounds));
+  //console.log('Replace with : ' + bcrypt.hashSync(password, saltRounds));
 
-    const user = users.find(u => u.username === username);
-    if (user) {
-        if (bcrypt.compareSync(password, user.password)) {
-            console.log("Passowrd is ok -> canLogIn");
-            const { password, ...userWithoutPassword } = user;
-            return userWithoutPassword;
-        }
+  const user = users.find(u => u.username === username);
+  if (user) {
+    if (bcrypt.compareSync(password, user.password)) {
+      console.log("Password is ok -> canLogIn");
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
     }
+  }
 
-    /*bcrypt.hash('password', saltRounds, (err, hash) => {
+  /*bcrypt.hash('password', saltRounds, (err, hash) => {
         console.log('Hash of "' + password + '" is : ' + hash);
     })*/
 
-    // const user = users.find(u => u.username === username && u.password === password);
-    //const user = users.find(u => u.username === username && u.password === bcrypt.compare(password, u.password));
-    /*if (user) {
+  // const user = users.find(u => u.username === username && u.password === password);
+  //const user = users.find(u => u.username === username && u.password === bcrypt.compare(password, u.password));
+  /*if (user) {
         const { password, ...userWithoutPassword } = user;
         return userWithoutPassword;
     }*/
 }
 
 async function register(user) {
-    await populateUsers();
-    const rules = {
-        username: "required",
-        // for multiple rules
-        email: "required|email",
-        firstname: "required",
-        lastname: "required",
-        password: "required|confirmed",
-        password_confirmation: "required",
-        type: "required|digits_between:0,1",
+  await populateUsers();
+  const rules = {
+    username: "required",
+    // for multiple rules
+    email: "required|email",
+    firstname: "required",
+    lastname: "required",
+    password: "required|confirmed",
+    password_confirmation: "required",
+    type: "required|digits_between:0,1"
+  };
+
+  const messages = {
+    // custom message for based rules
+    required: "Le champ :attr est requis",
+    email: "L'adresse email n'est pas valide"
+  };
+
+  const v = Validator.make(user, rules, messages);
+
+  if (v.fails()) {
+    const errors = v.getErrors();
+    console.log(errors);
+    const data = {
+      success: false,
+      errors: errors
     };
 
-    const messages = {
-        // custom message for based rules
-        required: "Le champ :attr est requis",
-        email: "L'adresse email n'est pas valide",
+    return data;
+  }
+
+  // Recherche d'un compte déjà existant avec username
+  const userEx = users.find(u => u.username === user.username);
+  if (userEx) {
+    const data = {
+      success: false,
+      errors: {
+        username: "Ce nom d'utilisateur est déjà utilisé."
+      }
     };
+    return data;
+  }
 
-    const v = Validator.make(user, rules, messages);
+  const userExEmail = users.find(u => u.email === user.email);
+  if (userExEmail) {
+    const data = {
+      success: false,
+      errors: {
+        email: "Cette adresse email est déjà utilisée."
+      }
+    };
+    return data;
+  }
 
-    if (v.fails()) {
-        const errors = v.getErrors();
-        console.log(errors);
-        const data = {
-            success: false,
-            errors: errors,
-        };
+  // Création du compte
+  // bcrypt.hashSync(password, saltRounds)
 
-        return data;
-    }
+  const date = new Date();
 
-    // Recherche d'un compte déjà existant avec username
-    const userEx = users.find(u => u.username === user.username);
-    if (userEx) {
-        const data = {
-            success: false,
-            errors: {
-                username: "Ce nom d'utilisateur est déjà utilisé.",
-            },
-        };
-        return data;
-    }
+  (user.created_at = date),
+    (user.updated_at = date),
+    (user.password = bcrypt.hashSync(user.password, saltRounds));
 
-    const userExEmail = users.find(u => u.email === user.email);
-    if (userExEmail) {
-        const data = {
-            success: false,
-            errors: {
-                email: "Cette adresse email est déjà utilisée.",
-            },
-        };
-        return data;
-    }
+  const { password_confirmation, ...insert } = user;
+  const { password, type, email, firstname, lastname, ...profile } = insert;
+  console.log("USER : " + insert);
+  console.log("PROFILE : " + profile);
 
-    // Création du compte
-    // bcrypt.hashSync(password, saltRounds)
+  let promise = await registerUser(insert);
+  let promiseP = await registerProfile(profile);
 
-    const date = new Date();
-
-    (user.created_at = date), (user.updated_at = date), (user.password = bcrypt.hashSync(user.password, saltRounds));
-
-    const { password_confirmation, ...insert } = user;
-    console.log(insert);
-
-    let promise = await registerUser(insert);
-
-    if(promise) {
-        const data = {
-            success: true
-        };
-        return data;
-    } else {
-        const data = {
-            success: false,
-            errors: {
-                register: 'Erreur lors de l\'inscription.'
-            }
-        };
-        return data;
-    }
-    
+  if (promise && promiseP) {
+    const data = {
+      success: true
+    };
+    return data;
+  } else {
+    const data = {
+      success: false,
+      errors: {
+        register: "Erreur lors de l'inscription."
+      }
+    };
+    return data;
+  }
 }
 
 async function getAll() {
-    await populateUsers();
-    return users.map(u => {
-        const { password, ...userWithoutPassword } = u;
-        return userWithoutPassword;
-    });
+  await populateUsers();
+  return users.map(u => {
+    const { password, ...userWithoutPassword } = u;
+    return userWithoutPassword;
+  });
 }
 
 async function getUserByName(username) {
-    await populateUsers();
-    const user = users.find(u => u.username === username);
-    if (user) {
-        console.log("getUserByName found " + user.username);
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
+  await populateUsers();
+  const user = users.find(u => u.username === username);
+  user.fresh = user.created_at == user.updated_at ? true : false;
+  if (user) {
+    console.log("getUserByName found " + user.username);
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+}
+
+async function getProfileByName(username) {
+  await populateUsers();
+  await populateProfiles();
+  const user = users.find(u => u.username === username);
+  if (user) {
+    console.log("getUserByName (profile ver.) found " + user.username);
+    const profile = profiles.find(p => p.username === username);
+    if (profile) {
+      console.log("getProfileByName found " + profile.username);
+      // Add some basic fields
+      profile.firstname = user.firstname;
+      profile.lastname = user.lastname;
+      const { id, username, ...profileWithoutFields } = profile;
+      return profileWithoutFields;
     }
+  }
+}
+
+async function updateProfileByName(name, user) {
+  const rules = {
+    birthdate: "string",
+    location: "string",
+    job: "string",
+    formation: "string"
+  };
+
+  const messages = {
+    // custom message for based rules
+    required: "Le champ :attr est requis"
+  };
+
+  const v = Validator.make(user, rules, messages);
+
+  if (v.fails()) {
+    const errors = v.getErrors();
+    console.log(errors);
+    const data = {
+      success: false,
+      errors: errors
+    };
+
+    return data;
+  }
+
+  let profileData = "";
+
+  // Recherche d'un profil
+  await getProfileByName(name).then(profile => {
+    if (profile) {
+      profileData = profile;
+    }
+  });
+
+  // console.log('PROFILE DATA : ' + JSON.stringify(profileData));
+
+  if (!profileData) {
+    const data = {
+      success: false,
+      errors: {
+        username: "Ce profil n'existe pas."
+      }
+    };
+    return data;
+  }
+
+  // Si on est ici c'est qu'on a un profil, donc on va le mettre à jour.
+
+  user.updated_at = new Date();
+
+  const { firstname, lastname, username, ...updated } = user;
+
+  console.log("THIS WILL BE UPDATED IN DDB : " + JSON.stringify(updated));
+
+  let promise = await updateProfile(name, updated);
+
+  if (promise) {
+    const data = {
+      success: true
+    };
+    return data;
+  } else {
+    const data = {
+      success: false,
+      errors: {
+        register: "Erreur lors de la mise à jour du profil."
+      }
+    };
+    return data;
+  }
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -1,35 +1,72 @@
-﻿const express = require('express');
+﻿const express = require("express");
 const router = express.Router();
-const userService = require('./user.service');
+const userService = require("./user.service");
 
 // routes
-router.post('/authenticate', authenticate);
-router.post('/register', register);
-router.get('/:name', getUserByName);
-router.get('/', getAll);
+router.post("/authenticate", authenticate);
+router.post("/register", register);
+router.get("/:name", getUserByName);
+router.get("/", getAll);
+// profile
+router.get("/:name/profile", getProfileByName);
+router.put("/:name/profile", updateProfileByName);
 
 module.exports = router;
 
 function authenticate(req, res, next) {
-    userService.authenticate(req.body)
-        .then(user => user ? res.json({ authdata: user }) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => next(err));
+  userService
+    .authenticate(req.body)
+    .then(user =>
+      user
+        ? res.json({ authdata: user })
+        : res.status(400).json({ message: "Username or password is incorrect" })
+    )
+    .catch(err => next(err));
 }
 
 function register(req, res, next) {
-    userService.register(req.body)
-        .then(data => data.success ? res.json({ data }) : res.status(400).json({ data }))
-        .catch(err => next(err));
+  userService
+    .register(req.body)
+    .then(data =>
+      data.success ? res.json({ data }) : res.status(400).json({ data })
+    )
+    .catch(err => next(err));
 }
 
 function getAll(req, res, next) {
-    userService.getAll()
-        .then(users => res.json(users))
-        .catch(err => next(err));
+  userService
+    .getAll()
+    .then(users => res.json(users))
+    .catch(err => next(err));
 }
 
 function getUserByName(req, res, next) {
-    userService.getUserByName(req.params.name)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username not found' }))
-        .catch(err => next(err));
+  userService
+    .getUserByName(req.params.name)
+    .then(user =>
+      user
+        ? res.json(user)
+        : res.status(400).json({ message: "Username not found" })
+    )
+    .catch(err => next(err));
+}
+
+function getProfileByName(req, res, next) {
+  userService
+    .getProfileByName(req.params.name)
+    .then(profile =>
+      profile
+        ? res.json(profile)
+        : res.status(400).json({ message: "Profile not found" })
+    )
+    .catch(err => next(err));
+}
+
+function updateProfileByName(req, res, next) {
+  userService
+    .updateProfileByName(req.params.name, req.body)
+    .then(data =>
+      data.success ? res.json({ data }) : res.status(400).json({ data })
+    )
+    .catch(err => next(err));
 }
